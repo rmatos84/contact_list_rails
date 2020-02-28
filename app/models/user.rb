e74aa9ffedc946076624b28f1c03057197ac848f
Romulo Matos <romulo@coredigital.com.br>
2020-02-28 11:31:26 -0300

    atualizando aula

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+
+    has_secure_password
+
+    VALIDE_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\d-]+)*\.[a-z]+\z/i
+
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :password, length: { minimum: 6 }
+    validates :email, presence: true, length: {maximum: 255},
+                                       format: { with: VALIDE_EMAIL_REGEX },
+                                       uniqueness: { case_sensitive: true }
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "static_pages#index"
@@ -6,5 +7,7 @@ Rails.application.routes.draw do
   get 'sobre', to: "static_pages#sobre"
   get 'contato', to: "static_pages#contato"
 
+  resources :contacts
+  resources :users, only: [:new, :create]
 
 end
